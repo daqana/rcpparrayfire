267d8b96d37819aedc20b501f9b73949c08996c5
Ralf Stubner <ralf.stubner@r-institute.com>
2017-11-13 17:06:48 +0100

    Update roxygen2 settings and generated files

---
+++ R/RcppArrayFire-package.R
@@ -0,0 +1,7 @@
+##' Rcpp integration for Arrayfire
+##'
+##' The RcppArrayFire package provides Rcpp integration for ArrayFire.
+##'
+##' @useDynLib RcppArrayFire, .registration = TRUE
+##' @importFrom Rcpp evalCpp
+"_PACKAGE"
