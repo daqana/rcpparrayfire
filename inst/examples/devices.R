659a6e7796ec9666c28844271b44fec3f0a56d7c
Ralf Stubner <ralf.stubner@r-institute.com>
2017-11-14 12:21:12 +0100

    Remove call to arrayfire_device_info

--- inst/examples/devices.R
+++ inst/examples/devices.R
@@ -3,7 +3,6 @@ library(RcppArrayFire)
 device_count <- arrayfire_count_device()
 for(i in 1:device_count) {
     arrayfire_set_device(1)
-    print(arrayfire_device_info())
     arrayfire_info()
 }
 
