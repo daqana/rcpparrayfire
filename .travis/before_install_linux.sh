30eae1e32f78c14709f4fecba8bffd9d07bc49ed
Ralf Stubner <ralf.stubner@r-institute.com>
2018-02-01 19:24:10 +0100

    Use Travis on OS X

---
+++ .travis/before_install_linux.sh
@@ -0,0 +1,3 @@
+wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_Linux_x86_64.sh
+sudo sh ArrayFire-v3.5.1_Linux_x86_64.sh --skip-license --exclude-subdir --prefix=/usr/local
+rm ArrayFire-v3.5.1_Linux_x86_64.sh
---
+++ .travis/before_install_osx.sh
@@ -0,0 +1,3 @@
+curl -O http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
+sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
+rm ArrayFire-v3.5.1_OSX.pkg
