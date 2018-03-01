b426f852f5f6db7acd086c2e6a33c3f468d7e9d2
Ralf Stubner <ralf.stubner@r-institute.com>
2018-03-01 14:06:33 +0100

    Install fontconfig before installing ArrayFire

--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -1,3 +1,4 @@
+sudo brew install fontconfig
 curl -O http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
 sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
 rm ArrayFire-v3.5.1_OSX.pkg
