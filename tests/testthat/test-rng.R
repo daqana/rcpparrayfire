e83a800717604c26c4d21f3d991abba7149304f0
Ralf Stubner <ralf.stubner@r-institute.com>
2018-03-01 15:40:44 +0100

    Also skip the new test on Travis Mac

--- tests/testthat/test-rng.R
+++ tests/testthat/test-rng.R
@@ -27,6 +27,7 @@ test_that("setting seed produces identical normaly distributed numbers", {
 })
 
 test_that("numbers are generated in the correct range", {
+    skip_if(identical(Sys.getenv("TRAVIS_OS_NAME"), "osx"), message = "On Travis Mac")
     arrayfire_set_seed(seed)
     u <- fastRunif(1e4, min = 10, max = 12)
     expect_true(all(u >= 10) && all(u < 12))
