fdb9aaf0596111f104a0f19568685209724938b0
Ralf Stubner <ralf.stubner@r-institute.com>
2018-03-01 15:22:28 +0100

    Also install glfw via brew

--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -1,4 +1,4 @@
-brew install fontconfig
+brew install fontconfig glfw
 curl -O http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
 sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
 rm ArrayFire-v3.5.1_OSX.pkg
