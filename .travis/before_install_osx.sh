584b471574262db758d0df026df97a18235fc0fb
Ralf Stubner <ralf.stubner@daqana.com>
2018-03-12 13:08:48 +0100

    Test AF installation on Travis Mac

--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -2,3 +2,13 @@ brew install fontconfig glfw
 curl -O http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_OSX.pkg
 sudo installer -pkg ArrayFire-v3.5.1_OSX.pkg -target /
 rm ArrayFire-v3.5.1_OSX.pkg
+
+cat <<EOF > minimal.cpp
+#include <arrayfire.h>
+int main() {
+    af_info();
+}
+EOF
+
+clang++ minimal.cpp -laf -o minimal
+./minimal
