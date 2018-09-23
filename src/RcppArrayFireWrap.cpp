39358593595e044d7964a522ec1ca1db4eb57e22
kafku <kazunoko93@gmail.com>
2018-09-23 09:35:36 +0900

    implement wrap() for sparse af::array

--- src/RcppArrayFireWrap.cpp
+++ src/RcppArrayFireWrap.cpp
@@ -23,57 +23,33 @@
 #include <RcppArrayFireWrap.h>
 
 namespace RcppArrayFire{
-    SEXP af_wrap( const af::array& object ){
+    SEXP wrap_af_impl( const af::array& object ){
         ::Rcpp::RObject x;
 
         switch(object.type()){
-        case f64:
-            x = wrap_array<double>( object ) ;
-            break;
-        case c64:
-            x = wrap_array<std::complex<double>>( object ) ;
-            break;
-        case f32:
-            x = wrap_array<float>( object ) ;
-            break;
-        case c32:
-            x = wrap_array<std::complex<float>>( object ) ;
-            break;
-        case s32:
-            x = wrap_array<int>( object ) ;
-            break;
-        case u32:
-            x = wrap_array<unsigned int>( object ) ;
-            break;
-        default:
-            Rcpp::stop("Unsopprted data type");
+            case f64:
+                x = wrap_array<double>( object ) ;
+                break;
+            case c64:
+                x = wrap_array<std::complex<double>>( object ) ;
+                break;
+            case f32:
+                x = wrap_array<float>( object ) ;
+                break;
+            case c32:
+                x = wrap_array<std::complex<float>>( object ) ;
+                break;
+            case s32:
+                x = wrap_array<int>( object ) ;
+                break;
+            case u32:
+                x = wrap_array<unsigned int>( object ) ;
+                break;
+            default:
+                Rcpp::stop("Unsopprted data type");
+                break;
         }
-        //NOTE:there is no af::sparse() in the current open source version of arrayfire
-        //if(object.issparse() == true){
-        //    const int  RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
 
-        //    IntegerVector dim = IntegerVector::create( object.dims(0), object.dims(1) );
-
-        //    // copy the data into R objects
-        //    Vector<RTYPE> x(object.device<T>(), object.device<T>() + object.nonzeros() ) ;
-        //    IntegerVector i(/*begin of the row indices of object*/, /*end*/);
-        //    IntegerVector p(/*begin of the col indices of object*/, /*end*/);
-
-        //    std::string klass ;
-        //    switch( RTYPE ){
-        //        case REALSXP: klass = "dgCMatrix" ; break ;
-        //        // case INTSXP : klass = "igCMatrix" ; break ; class not exported
-        //        case LGLSXP : klass = "lgCMatrix" ; break ;
-        //        default:
-        //            throw std::invalid_argument( "RTYPE not matched in conversion to sparse matrix" ) ;
-        //    }
-        //    S4 s(klass);
-        //    s.slot("i")   = i;
-        //    s.slot("p")   = p;
-        //    s.slot("x")   = x;
-        //    s.slot("Dim") = dim;
-        //    return s;
-        //}
         return x;
     }
 } /* namespace RcppArrayFire */
@@ -88,8 +64,8 @@ namespace Rcpp{
     }
 
     template <> SEXP wrap (const af::array& data ){
-        ::Rcpp::RObject x = ::RcppArrayFire::af_wrap( data );
-        if (data.numdims() > 1)
+        ::Rcpp::RObject x = ::RcppArrayFire::wrap_af_impl( data );
+        if (data.issparse() == false && data.numdims() > 1)
             x.attr("dim") = wrap(data.dims());
         return x;
     }
