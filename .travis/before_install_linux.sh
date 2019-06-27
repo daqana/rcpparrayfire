4fad51040e71ff53a0ab399d97971bc65d58a96e
Ralf Stubner <ralf.stubner@gmail.com>
2019-06-27 22:35:30 +0200

    Download AF installer silently

--- .travis/before_install_linux.sh
+++ .travis/before_install_linux.sh
@@ -1,4 +1,4 @@
-wget http://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_Linux_x86_64.sh
+wget --quiet http://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_Linux_x86_64.sh
 sudo sh ArrayFire-v3.6.4_Linux_x86_64.sh --skip-license  --exclude-subdir --prefix=/usr/local
 rm ArrayFire-v3.6.4_Linux_x86_64.sh
 sudo ldconfig /usr/local/lib64
--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -1,4 +1,4 @@
 brew install fontconfig glfw
-curl -O https://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_OSX_x86_64.pkg
+curl -SO https://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_OSX_x86_64.pkg
 sudo installer -pkg ArrayFire-v3.6.4_OSX_x86_64.pkg -target /
 rm ArrayFire-v3.6.4_OSX_x86_64.pkg
