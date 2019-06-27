f86679aca460e95e47a5f6c24c3d3d91198d094f
Ralf Stubner <ralf.stubner@gmail.com>
2019-06-27 21:37:35 +0200

    Correct and clen-up AF installation on Mac OS

--- .travis/before_install_osx.sh
+++ .travis/before_install_osx.sh
@@ -1,31 +1,4 @@
 brew install fontconfig glfw
 curl -O https://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_OSX_x86_64.pkg
-sudo installer -pkg ArrayFire-v3.6.4_OSX.pkg -target /
-rm ArrayFire-v3.6.4_OSX.pkg
-
-cat <<EOF > minimal.cpp
-#include <arrayfire.h>
-#include <stdio.h>
-
-int main() {
-  unsigned int count;
-  af_get_backend_count(&count);
-  printf("backends: %d\n", count);
-  af_info();
-  return 0;
-}
-EOF
-
-echo "With afopencl"
-clang++ minimal.cpp -lafopencl -o minimal
-./minimal
-otool -L minimal
-echo "With af"
-clang++ minimal.cpp -laf -o minimal
-./minimal
-otool -L minimal
-echo "Removing temp files"
-rm -f minimal minimal.cpp
-otool -L /opt/arrayfire/lib/libaf.3.dylib
-otool -L /opt/arrayfire/lib/libafopencl.3.dylib
-otool -L /opt/arrayfire/lib/libafcpu.3.dylib
+sudo installer -pkg ArrayFire-v3.6.4_OSX_x86_64.pkg -target /
+rm ArrayFire-v3.6.4_OSX_x86_64.pkg
