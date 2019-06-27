088dbf00798e8e0a81a80bc11437e1cca9ee3236
Ralf Stubner <ralf.stubner@gmail.com>
2019-06-27 21:00:27 +0200

    Updat AF on Linux

--- .travis/before_install_linux.sh
+++ .travis/before_install_linux.sh
@@ -1,3 +1,3 @@
-wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-v3.5.1_Linux_x86_64.sh
-sudo sh ArrayFire-v3.5.1_Linux_x86_64.sh --skip-license --exclude-subdir --prefix=/usr/local
-rm ArrayFire-v3.5.1_Linux_x86_64.sh
+wget http://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_Linux_x86_64.sh
+sudo sh ArrayFire-v3.6.4_Linux_x86_64.sh --skip-license
+rm ArrayFire-v3.6.4_Linux_x86_64.sh
