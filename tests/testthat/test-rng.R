64f2314321241a0de3a1e0b56760d73068ba8c9f
Ralf Stubner <ralf.stubner@gmail.com>
2019-06-27 22:08:47 +0200

    Enable remaining tests on Travis MacOS

--- tests/testthat/test-rng.R
+++ tests/testthat/test-rng.R
@@ -3,13 +3,11 @@ context("rng")
 seed <- 12345678901234567890
 
 test_that("setting and getting the random seed works", {
-    skip_if(identical(Sys.getenv("TRAVIS_OS_NAME"), "osx"), message = "On Travis Mac")
     arrayfire_set_seed(seed)
     expect_equal(seed, arrayfire_get_seed())
 })
 
 test_that("setting seed produces identical uniformly distributed numbers", {
-    skip_if(identical(Sys.getenv("TRAVIS_OS_NAME"), "osx"), message = "On Travis Mac")
     arrayfire_set_seed(seed)
     u1 <- fastRunif(10)
     arrayfire_set_seed(seed)
@@ -18,7 +16,6 @@ test_that("setting seed produces identical uniformly distributed numbers", {
 })
 
 test_that("setting seed produces identical normaly distributed numbers", {
-    skip_if(identical(Sys.getenv("TRAVIS_OS_NAME"), "osx"), message = "On Travis Mac")
     arrayfire_set_seed(seed)
     n1 <- fastRnorm(10)
     arrayfire_set_seed(seed)
@@ -27,7 +24,6 @@ test_that("setting seed produces identical normaly distributed numbers", {
 })
 
 test_that("numbers are generated in the correct range", {
-    skip_if(identical(Sys.getenv("TRAVIS_OS_NAME"), "osx"), message = "On Travis Mac")
     arrayfire_set_seed(seed)
     u <- fastRunif(1e4, min = 10, max = 12)
     expect_true(all(u >= 10) && all(u < 12))
